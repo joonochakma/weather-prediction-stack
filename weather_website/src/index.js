95d15ca598faa165f20b84647ee9acbd83122beb
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-10-09 16:26:31 +1100

    add file structure

--- weather_website/src/index.js
+++ weather_website/src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './Header';
+import Home from './pages/home/Home';
+import Footer from './Footer';
+import Heatwave from './pages/Models/Heatwave';
+import Rainfall from './pages/Models/Rainfall';
+import Weather from './pages/Models/Weather';
+import Temperature from './pages/Models/Temperature';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
-    <App />
+    <Home />
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<pages/>}>
+          <Route index element={<Home />} />
+          <Route path="Temperature" element={<Temperature />} />
+          <Route path="Rainfall" element={<Rainfall />} />
+          <Route path="Heatwave" element={<Heatwave />} />
+          <Route path="Weather" element={<Weather />} />
+
+          
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    <Footer/>
   </React.StrictMode>
 );
 
