77744d4bbd8df487ae9cc28be900a01c1bb4ef36
anmrry <104349778@studeny.swin.edu.au>
2024-10-23 15:35:29 +1100

    FAQ implemented

--- weather_website/src/pages/faq/FAQ.js
+++ weather_website/src/pages/faq/FAQ.js
@@ -1,10 +1,49 @@
+import React from 'react';
+import { Box, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function FAQ() {
+  const faqs = [
+    {
+      question: "What is this website about?",
+      answer: "This website provides weather-related information including temperature, weather condition, rainfall prediction, and more."
+    },
+    {
+      question: "Where can I find temperature data?",
+      answer: "Temperature data is available under the 'Temperature' section in the models dropdown menu."
+    },
+    {
+      question: "What is forecasting?",
+      answer: "A calculation or estimate of future events, especially upcoming weather."
+    }
+  ];
+
+  const menuItems = [
+    { name: 'Rainfall', link: '/rainfall' },
+    { name: 'Temperature', link: '/temperature' },
+    { name: 'Weather Conditions', link: '/weather' },
+    { name: 'Heatwave', link: '/heatwave' }
+  ]
     return (
-      <div className="App">
-        <h1>FAQ</h1>
-        <p>Here are some frequently asked questions.</p>
-      </div>
+      <Box sx={{ padding: '20px' }}>
+      <Typography variant="h4" gutterBottom>
+        FAQ
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Here are some frequently asked questions:
+      </Typography>
+      // Display FAQs using Accordion component
+      {faqs.map((faq, index) => (
+        <Accordion key={index} sx={{ marginBottom: '10px' }}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>{faq.question}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{faq.answer}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </Box>
     );
   }
   
