86138779fe78d7aab98d1e030ab7a67805e8710a
anmrry <104349778@studeny.swin.edu.au>
2024-11-01 12:39:53 +1100

    weather page and responsiveness updated

--- weather_website/src/pages/weather/Weather.js
+++ weather_website/src/pages/weather/Weather.js
@@ -1,10 +1,11 @@
-
-
+import React from 'react';
+import './Weather.css';
 
 function Weather() {
   return (
-    <div className="App">
-
+    <div className="weather-container">
+            <h2 className="model_title">Weather Conditions Prediction</h2>
+            <p className="model_description">Classify weather conditions based on provide data.</p>
     </div>
   );
 }
